b48d10da410e2cd62bdeeb0de4d5686c9cbcf864
John Carlson <john.carlson3@sbcglobal.net>
2014-02-18 02:15:01 -0600

    added includes for test

---
+++ src/testincludes.hpp
@@ -0,0 +1,14 @@
+#include "ActionEvent.hpp"
+#include "ActionListener.hpp"
+#include "Window.hpp"
+#include </opt/local/include/glm/vec3.hpp>
+#include </opt/local/include/glm/vec4.hpp>
+#include </opt/local/include/glm/mat4x4.hpp>
+#include </opt/local/include/glm/gtc/matrix_transform.hpp>
+#include </opt/local/include/glm/gtc/type_ptr.hpp>
+#include "Font.hpp"
+#include "ShaderProgram.hpp"
+#include "Button.hpp"
+#include "Label.hpp"
+#include "TextField.hpp"
+#include <iostream>
